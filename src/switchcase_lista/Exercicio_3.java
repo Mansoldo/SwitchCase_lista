94f8f0a061cda026f7655de89c76ad71390ac8d6
Titio-Lele <gp-alexandresantos@uol.com.br>
2018-09-15 17:53:16 -0300

    commit titio_lele

--- src/switchcase_lista/Exercicio_3.java
+++ src/switchcase_lista/Exercicio_3.java
@@ -5,17 +5,56 @@
  */
 package switchcase_lista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author diego.amalmeida
  */
 public class Exercicio_3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		int qtdNota100 = 0;
+		int qtdNota50 = 0;
+		int valor = 0;
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o valor: ");
+		valor = entrada.nextInt();
+
+		switch (valor % 100) {
+		case (0):
+
+			qtdNota100 = valor / 100;
+			break;
+
+		default:
+			qtdNota100 = valor / 100;
+			valor = valor % 100;
+
+			break;
+		}
+		switch (valor % 50) {
+		case (0):
+			qtdNota50 = valor / 50;
+			break;
+
+		default:
+			qtdNota50 = valor / 50;
+			valor = valor % 50;
+			break;
+
+		}
+		entrada.close();
+
+		System.out.println("Quantidade de notas de 100: " + qtdNota100);
+		System.out.println("Quantidade de notas de 50: " + qtdNota50);
+	}
+
 }
